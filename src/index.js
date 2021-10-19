4f90d310089040754f1ae498b64a2d9d0ea2fc79
Steve Yardumian <steveyardumian@gmail.com>
2021-10-18 21:23:36 -0400

    add web push to sound effects command

--- src/index.js
+++ src/index.js
@@ -1,58 +1,92 @@
-const SoundEffects = require('./soundEffects');
-const config = require('./.env.js')
-
 const tmi = require('tmi.js');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const express = require('express');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
-const ws = require('ws')
-const wss = new ws.WebSocketServer({ port: 8888 });
-let thisSocket
+const SoundEffects = require('./soundEffects');
+const config = require('./.env.js');
 
-wss.on('connection', function connection(socket) {
-  console.log('* web socket connected')
-  socket.send('{ "connected": true }')
-  thisSocket = socket
-});
+open({
+  filename: config.dbLocation,
+  driver: sqlite3.cached.Database
+}).then((db) => {
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) return // Ignore messages from the bot
-  if (context["message-type"] !== 'chat') return // ignore messages that are not chat type
+  // ------ START HTTPS SERVER
+  const app = express();
+  app.use(cors());
+  app.use(express.json())
 
-  // Remove whitespace from edges of chat message
-  const chatMsg = msg.trim().toLowerCase();
+  const httpsServer = https.createServer({
+    key: fs.readFileSync('/Users/steveyardumian/Downloads/push_jeantbot_site.key'), // fs.readFileSync('/home/syardumi/ssl/push_jeantbot_site.key'),
+    cert: fs.readFileSync('/Users/steveyardumian/Downloads/push_jeantbot_site.crt') // fs.readFileSync('/home/syardumi/ssl/push_jeantbot_site.crt'),
+  }, app);
 
-  // console.log(context)
+  app.post('/notifications/subscribe', async (req, res) => {
+    const subscription = req.body
+  
+    // save user to DB
+    let result = await db.get('SELECT * FROM push_subscriptions WHERE endpoint = ?', subscription.endpoint)
+    if (!result) {
+      await db.run(
+        'INSERT INTO push_subscriptions (endpoint, keys_p256dh, keys_auth) VALUES (?, ?, ?)',
+        subscription.endpoint, subscription.keys.p256dh, subscription.keys.auth
+      )
+    }
+  
+    res.status(200).json({'success': true})
+  });
 
-  if (config.modules.soundEffects) {
-    SoundEffects(target, context, chatMsg, client, thisSocket)
+  httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+  });
+
+  // app.listen(9000, () => console.log('The server has been started on the port 9000'))
+
+  // ------ START TWITCH LISTEN
+  // Called every time a message comes in
+  async function onMessageHandler (target, context, msg, self) {
+    if (self) return // Ignore messages from the bot
+    if (context["message-type"] !== 'chat') return // ignore messages that are not chat type
+
+    // Remove whitespace from edges of chat message
+    const chatMsg = msg.trim().toLowerCase();
+
+    // console.log(context)
+
+    if (config.modules.soundEffects) {
+      await SoundEffects(target, context, chatMsg, client, db)
+    }
   }
-}
 
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
+  // Called every time the bot connects to Twitch chat
+  function onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+  }
 
-// Create a client with our options
-const client = new tmi.client({
-  options: {
-    debug: false
-  },
-  connection: {
-      reconnect: true,
-      secure: true
-  },
-  identity: {
-    username: config.oauthUsername,
-    password: config.oauthPassword
-  },
-  channels: config.channels
-});
+  // Create a client with our options
+  const client = new tmi.client({
+    options: {
+      debug: true
+    },
+    connection: {
+        reconnect: true,
+        secure: true
+    },
+    identity: {
+      username: config.oauthUsername,
+      password: config.oauthPassword
+    },
+    channels: config.channels
+  });
 
-// // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
+  // // Register our event handlers (defined below)
+  client.on('message', onMessageHandler);
 
-client.on('connected', onConnectedHandler);
+  client.on('connected', onConnectedHandler);
 
-// // Connect to Twitch:
-client.connect();
+  // // Connect to Twitch:
+  client.connect();
+})
--- src/soundEffects.js
+++ src/soundEffects.js
@@ -1,14 +1,35 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const webpush = require('web-push');
+const config = require('./.env.js');
 
-const SoundEffects = (target, context, chatMsg, client, thisSocket) => {
+webpush.setVapidDetails(config.vapid.subject, config.vapid.publicKey, config.vapid.privateKey);
+
+const SoundEffects = async (target, context, chatMsg, client, db) => {
     if (chatMsg.includes('!iamjeant')) {
-        const files = fs.readdirSync(path.join(__filename, '../assets'))
-        const file = files[Math.floor(Math.random() * files.length)]
+        const files = fs.readdirSync(path.join(__filename, '../assets'));
+        const file = files[Math.floor(Math.random() * files.length)];
 
-        // send a web socket push to our website with a random sound clip
-        const msg = `{ "audioSrc": "/audio/${file}" }`
-        if (thisSocket) thisSocket.send(msg)
+        // send a broadcast push to all clients
+        const msg = JSON.stringify({
+            audioSrc: `/audio/${file}`
+        });
+        const result = await db.all('SELECT * FROM push_subscriptions');
+        if (result && result.length) {
+            const pushPromises = []
+            result.forEach((record) => {
+                pushPromises.push(webpush.sendNotification({
+                    endpoint: record.endpoint,
+                    keys: {
+                        p256dh: record.keys_p256dh,
+                        auth: record.keys_auth
+                    }
+                }, msg).catch(async (e) => {
+                    await db.run('DELETE FROM push_subscriptions WHERE endpoint = ?', e.endpoint)
+                }));
+            });
+            await Promise.all(pushPromises)
+        }
 
         console.log(`* Executed ${chatMsg} command. Sent '${msg}'`);
     }
