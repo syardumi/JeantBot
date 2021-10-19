24ca4654ee45ccdcce62ece39274e2d46c312f66
Steve Yardumian <steve@360mediadirect.com>
2021-10-19 14:40:02 -0400

    move permissioning and cooldown into the !iamjeant command

--- src/soundEffects.js
+++ src/soundEffects.js
@@ -1,15 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const webpush = require('web-push');
-const config = require('./.env.js');
+const addMilliseconds = require('date-fns/addMilliseconds')
 
+const config = require('./.env.js');
 webpush.setVapidDetails(config.vapid.subject, config.vapid.publicKey, config.vapid.privateKey);
 
-const SoundEffects = async (target, context, chatMsg, client, db) => {
-    const isVipOrModUser = (context['badges'] && context['badges']['broadcaster'] === '1') || (context['badges'] && context['badges']['vip'] === '1') || context['mod'] === true || context['username'] === 'thefinaledge'
-    if (!isVipOrModUser) return
+const COOLDOWN_IN_MS = process.argv && process.argv.length > 2 ? process.argv[2].split('=')[1] * 1000 : 30000
+let lastRunDateTimestamp
 
+const SoundEffects = async (target, context, chatMsg, client, db) => {
     if (chatMsg.includes('!iamjeant')) {
+        // ONLY VIPS AND MODS CAN RUN THIS CMD
+        const isVipOrModUser = (context['badges'] && context['badges']['broadcaster'] === '1') || (context['badges'] && context['badges']['vip'] === '1') || context['mod'] === true || context['username'] === 'thefinaledge'
+        if (!isVipOrModUser) return
+
+        // CMD CAN ONLY RUN AFTER COOLDOWN (via argument or defaults to 30s)
+        if (lastRunDateTimestamp && Date.now() < addMilliseconds(new Date(lastRunDateTimestamp), COOLDOWN_IN_MS)) {
+          const timeLeft = (addMilliseconds(new Date(lastRunDateTimestamp), COOLDOWN_IN_MS) - Date.now()) / 1000
+          console.log(`tried command too soon; ${timeLeft} sec left`)
+          return
+        }
+        lastRunDateTimestamp = Date.now()
+
+        // get a random audio file
         const files = fs.readdirSync(path.join(__filename, '../assets'));
         const file = files[Math.floor(Math.random() * files.length)];
 
